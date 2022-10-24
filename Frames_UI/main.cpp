7699f5f5476cf7efc1220ebe1c0e450a3e79f644
Junwoo Hwang <junwoo@auterion.com>
2022-10-24 14:27:02 +0200

    Change build directory and fix QML error not showing any GUI

--- Frames_UI/main.cpp
+++ Frames_UI/main.cpp
@@ -1,27 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-#include <QLocale>
-#include <QTranslator>
-
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
     QGuiApplication app(argc, argv);
-
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "Frames_UI_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            app.installTranslator(&translator);
-            break;
-        }
-    }
-
     QQmlApplicationEngine engine;
+
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
@@ -30,5 +17,6 @@ int main(int argc, char *argv[])
     }, Qt::QueuedConnection);
     engine.load(url);
 
+    qDebug() << "Hi!!";
     return app.exec();
 }
--- Frames_UI/main.qml
+++ Frames_UI/main.qml
@@ -1,6 +1,9 @@
 import QtQuick 2.15
 import QtQuick.Window 2.15
 
-Text {
-    text: "Hello World! !!!"
+Window {
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("Hello World")
 }
